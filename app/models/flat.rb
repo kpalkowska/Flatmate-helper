8c0a3d1a44fe2e510248b3dc6102d2df2cbfed4e
Dariusz Buszman <dbuszman@hotmail.com>
2016-05-18 00:08:13 +0200

    Moved geocoder for address to right place in code

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,7 @@
 class Flat < ActiveRecord::Base
   validates :address, presence: true, length: { maximum: 70 }
   validates :total_charges, presence: true
-end
 
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,2 @@
 class Map < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
 end
